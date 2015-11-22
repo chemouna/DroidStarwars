d1946faa3207e7e5c25e28f54f99dec42b267ca1
Mouna Cheikhna <cheikhnamouna@gmail.com>
2015-11-22 08:25:27 +0100

    Add dependencies and styles.

---
+++ mobile/src/main/java/com/mounacheikhna/ctc/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mounacheikhna.ctc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cheikhnamouna on 11/21/15.
+ */
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+
+}
---
+++ mobile/src/main/java/com/mounacheikhna/ctc/fragments/ItemFragment.java
@@ -0,0 +1,14 @@
+package com.mounacheikhna.ctc.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by cheikhnamouna on 11/21/15.
+ */
+public class ItemFragment extends Fragment {
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
